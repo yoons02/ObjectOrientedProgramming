27b78bf0a58a70f7cde0ef80372761579842eb81
yoons <yoons@gim-yunseong-uiui-MacBook-Pro.local>
2023-03-07 19:24:09 +0900

    1주차 예제

---
+++ src/InputOutput01.java
@@ -0,0 +1,8 @@
+public class InputOutput01 {
+    public static void main(String[] args) {
+        String myName = "김윤성";
+        int myAge = 22;
+        System.out.print("이름 : \t " + myName + "\n");
+        System.out.print("나이 : \t " + myAge + "\n");
+    }
+}
---
+++ src/dataType.java
@@ -0,0 +1,12 @@
+public class dataType {
+    public static void main(String[] args) {
+        int a = 10;
+        short s = 2;
+        byte b = 6;
+        long i = 125362133223L;
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(i);
+    }
+}
