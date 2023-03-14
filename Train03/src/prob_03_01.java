706e62f03e971a8463da913bcde3a294f4d5e730
yoons <yoons@gim-yunseong-uiui-MacBook-Pro.local>
2023-03-15 00:48:48 +0900

    Modify Train03

--- Train03/src/prob_03_01.java
+++ Train03/src/prob_03_01.java
@@ -6,12 +6,14 @@ public class prob_03_01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("첫 번째 계산할 값을 입력하세요 : ");
-        int first = sc.nextInt();
+        double first = sc.nextInt();
         System.out.print("두 번쨰 계산할 값을 입력하세요 : ");
-        int second = sc.nextInt();
-        System.out.printf("%d + %d = %d\n", first, second, first+second) ;
-        System.out.printf("%d - %d = %d\n", first, second, first-second) ;
-        System.out.printf("%d / %d = %d\n", first, second, first/second) ;
-        System.out.printf("%d * %d = %d\n", first, second, first*second) ;
+        double second = sc.nextInt();
+        System.out.println();
+        System.out.println("계산 결과");
+        System.out.printf("%.2f + %.2f = %.2f\n", first, second, first+second) ;
+        System.out.printf("%.2f - %.2f = %.2f\n", first, second, first-second) ;
+        System.out.printf("%.2f / %.2f = %.2f\n", first, second, first/second) ;
+        System.out.printf("%.2f * %.2f = %.2f\n", first, second, first*second) ;
     }
 }
--- Train03/src/prob_03_02.java
+++ Train03/src/prob_03_02.java
@@ -6,9 +6,9 @@ public class prob_03_02 {
     public static void main(String[] args) {
         Scanner data = new Scanner(System.in);
         System.out.print("이름을 입력하세요 : ");
-        String name = data.next();
+        String name = data.nextLine();
         System.out.print("전화번호를 입력하세요 : ");
-        String phone_num = data.next();
+        String phone_num = data.nextLine();
 
         System.out.println("***************************************");
         System.out.println("       Welcome To Shopping Mall");
@@ -18,7 +18,7 @@ public class prob_03_02 {
         System.out.printf("1. 고객 정보 확인\t\t2. 장바구니 상품 목록 보기\n");
         System.out.printf("3. 장바구니 비우기\t\t4. 바구니에 항목 추가하기\n");
         System.out.printf("5. 장바구니 수정하기\t\t6. 장바구니 삭제하기\n");
-        System.out.println("7. 영수증 표시하기\t\t8. 종료\n");
+        System.out.printf("7. 영수증 표시하기\t\t8. 종료\n");
 
         System.out.println();
         System.out.print("메뉴 번호 선택 : ");
